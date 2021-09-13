9cb3b1a5e62f5ffb50e24f7abc0bd77bf7010f6b
BHJ <bhj7@kakao.com>
2021-09-13 20:04:28 +0900

    replace

--- InfoAlertCUL.podspec
+++ InfoAlertCUL.podspec
@@ -21,11 +21,11 @@ Pod::Spec.new do |s|
 TODO: Add long description of the pod here.
                        DESC
 
-  s.homepage         = 'https://github.com/bhj7@kakao.com/InfoAlertCUL'
+  s.homepage         = 'https://github.com/kingsjune/InfoAlertCUL'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'bhj7@kakao.com' => 'bhj7@kakao.com' }
-  s.source           = { :git => 'https://github.com/bhj7@kakao.com/InfoAlertCUL.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/kingsjune/InfoAlertCUL.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '9.0'
--- InfoAlertCUL/Classes/DeviceInfo.swift
+++ InfoAlertCUL/Classes/DeviceInfo.swift
@@ -9,7 +9,7 @@ import UIKit
 //import AdSupport
 
 open class DeviceInfo: NSObject {
-  
+    
     //MARK: 디바이스 제조사
     public static func makeName() -> String {
         return "Apple"
@@ -37,18 +37,18 @@ open class DeviceInfo: NSObject {
     
     //MARK: 스크린사이즈
     public static func screenSize() -> String {
-        let size = NSStringFromCGRect(UIScreen.main.bounds)
+        let size = NSCoder.string(for: UIScreen.main.bounds)
         return size
     }
     
-//        //MARK: DPI
-//        public static func dpi() -> String {
-//        }
-//
-//        //MARK: Density
-//        public static func density() -> String {
-//        }
-//
+    //        //MARK: DPI
+    //        public static func dpi() -> String {
+    //        }
+    //
+    //        //MARK: Density
+    //        public static func density() -> String {
+    //        }
+    //
     //MARK: 사용언어
     public static func useLanguage() -> String {
         guard let languge = Locale.current.languageCode else {
@@ -69,35 +69,35 @@ open class DeviceInfo: NSObject {
         return appVersion
     }
     
-//        //MARK: Appstore ID
-//        public static func AppstoreID() -> String {
-//        }
-//
-//        //MARK: IP (https://www.ipify.org 참고)
-//        public static func ip() -> String {
-//        }
+    //        //MARK: Appstore ID
+    //        public static func AppstoreID() -> String {
+    //        }
+    //
+    //        //MARK: IP (https://www.ipify.org 참고)
+    //        public static func ip() -> String {
+    //        }
     
-//    //MARK: IDFA
-//    public static func idfa() -> String {
-//        let myIDFA: String?
-//        if ASIdentifierManager.shared().isAdvertisingTrackingEnabled {
-//            myIDFA = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-//            return myIDFA!
-//        } else {
-//            myIDFA = nil
-//            return "Unknown"
-//        }
-//    }
+    //    //MARK: IDFA
+    //    public static func idfa() -> String {
+    //        let myIDFA: String?
+    //        if ASIdentifierManager.shared().isAdvertisingTrackingEnabled {
+    //            myIDFA = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    //            return myIDFA!
+    //        } else {
+    //            myIDFA = nil
+    //            return "Unknown"
+    //        }
+    //    }
     
-//    //MARK: IDFV
-//    public static func idfv() -> String {
-//    }
-//
-//    //MARK: WebView 의 User-Agent 정보
-//    public static func webViewUserAgent() -> String {
-//    }
-//
-//    //MARK: SKAdNetworkIdentifier 정보 보호 목록
-//    public static func skAdNetworkIdentifier() -> String {
-//    }
+    //    //MARK: IDFV
+    //    public static func idfv() -> String {
+    //    }
+    //
+    //    //MARK: WebView 의 User-Agent 정보
+    //    public static func webViewUserAgent() -> String {
+    //    }
+    //
+    //    //MARK: SKAdNetworkIdentifier 정보 보호 목록
+    //    public static func skAdNetworkIdentifier() -> String {
+    //    }
 }
